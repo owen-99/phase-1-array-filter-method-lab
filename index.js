5ae9d69fa99d63a3cce0626c88195990cf55c0de
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-14 09:00:49 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+const findMatching = (arr, str) => {
+  str = str.toLowerCase();
+  return arr.filter((item) => item.toLowerCase() == str);
+};
+
+console.log(findMatching(drivers, "Bobby"));
+
+const fuzzyMatch = (arr, str) => {
+  str = str.toLowerCase();
+  return arr.filter((item) => item.toLowerCase().substr(0, 3).includes(str));
+};
+
+console.log(fuzzyMatch(drivers, "Sa"));
+console.log(fuzzyMatch(drivers, "y"));
+console.log(fuzzyMatch(drivers, "mm"));
+
+const driverss = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+const matchName = (arr, str) => {
+  // properties: name and hometown. The
+  str = str.toLowerCase();
+  return arr.filter((obj) => obj.name.toLowerCase() == str);
+};
+
+console.log(matchName(driverss, "Bobby"));
